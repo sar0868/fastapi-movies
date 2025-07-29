7d31b011d44523d362b7255a9d586049f325170e
sar0868 <safarov-ar@yandex.ru>
2025-07-29 16:33:22 +0300

    add view root and links docs

--- moveis/main.py
+++ moveis/main.py
@@ -1,6 +1,20 @@
-from fastapi import FastAPI
+from fastapi import (
+    FastAPI,
+    Request,
+)
 
 
 app = FastAPI(
     title="Catalog movies",
 )
+
+
+@app.get("/")
+def get_root(request: Request):
+    docs_url = request.url.replace(
+        path="/docs",
+    )
+    return {
+        "message": "Catalog movies",
+        "docs": str(docs_url),
+    }
